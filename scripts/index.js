393390de9e13d97261d37a5ed170867f2346c519
Alina Kravchenko <krav-alina@yandexx.ru>
2022-05-24 18:11:32 +0300

    add changes for all the project

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+const popup = document.querySelector('.popup');
+const popupContainer = document.querySelector('.popup__container');
+const editButton = document.querySelector('.form__button_size_small');
+const closeButton = document.querySelector('.popup__close');
+const saveButton = document.querySelector('.popup__button');
+const formElement = document.querySelector('.popup__form');
+const titleElement = document.querySelector('.profile__title');
+const typeElement = document.querySelector('.profile__subtitle');
+const nameFieldElement = document.querySelector('.popup__input_name');
+const typeFieldElement = document.querySelector('.popup__input_type');
+
+editButton.addEventListener('click', function(){
+    popup.classList.add('popup_isOpen');
+    popupContainer.classList.add('popup__container_isOpen');
+    nameFieldElement.value = titleElement.textContent;
+    typeFieldElement.value = typeElement.textContent;
+});
+
+closeButton.addEventListener('click', function(){
+    popup.classList.remove('popup_isOpen');
+    popupContainer.classList.remove('popup__container_isOpen');
+});
+
+formElement.addEventListener('submit', function(event){
+    event.preventDefault();
+    titleElement.textContent = nameFieldElement.value;
+    typeElement.textContent = typeFieldElement.value;
+    popup.classList.remove('popup_isOpen');
+    popupContainer.classList.remove('popup__container_isOpen');
+});
+
