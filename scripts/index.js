6ef74ab31071f6ca15e24c67eb73b861214bd3c3
Alina Kravchenko <krav-alina@yandexx.ru>
2022-05-25 15:40:27 +0300

    add changes after 1st review

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,27 @@
 const popup = document.querySelector('.popup');
-const popupContainer = document.querySelector('.popup__container');
 const editButton = document.querySelector('.profile__button_size_small');
-const closeButton = document.querySelector('.popup__close');
+const closeButton = document.querySelector('.popup__close-button');
 const saveButton = document.querySelector('.popup__button');
 const formElement = document.querySelector('.popup__form');
 const titleElement = document.querySelector('.profile__title');
 const typeElement = document.querySelector('.profile__subtitle');
-const nameFieldElement = document.querySelector('.popup__input_name');
-const typeFieldElement = document.querySelector('.popup__input_type');
+const nameFieldElement = document.querySelector('.popup__input_type_name');
+const typeFieldElement = document.querySelector('.popup__input_type_occupation');
 
 editButton.addEventListener('click', function(){
-    popup.classList.add('popup_isOpen');
-    popupContainer.classList.add('popup__container_isOpen');
+    popup.classList.add('popup_open');
     nameFieldElement.value = titleElement.textContent;
     typeFieldElement.value = typeElement.textContent;
 });
 
 closeButton.addEventListener('click', function(){
-    popup.classList.remove('popup_isOpen');
-    popupContainer.classList.remove('popup__container_isOpen');
+    popup.classList.remove('popup_open');
 });
 
 formElement.addEventListener('submit', function(event){
     event.preventDefault();
     titleElement.textContent = nameFieldElement.value;
     typeElement.textContent = typeFieldElement.value;
-    popup.classList.remove('popup_isOpen');
-    popupContainer.classList.remove('popup__container_isOpen');
+    popup.classList.remove('popup_open');
 });
 
