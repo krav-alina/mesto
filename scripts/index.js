618194567d08b90aa98572bf779cb57958a39366
Alina Kravchenko <krav-alina@yandex.ru>
2022-08-16 19:41:15 +0300

    edit

--- scripts/index.js
+++ scripts/index.js
@@ -24,45 +24,64 @@ const initialCards = [
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
   ]; 
-
+//popup name 
 const popupName = document.getElementById('popupName');
-const popupPlace = document.getElementById('popupPlace');
 const editButtonSmall = document.querySelector('.profile__button_size_small');
-const editButtonBig = document.querySelector('.profile__button_size_big');
-const closeButton = document.querySelector('.popup__close-button');
+const closeButtonName = document.getElementById('closeButtonName');
 const saveButton = document.querySelector('.popup__button');
 const formElement = document.querySelector('.popup__form');
 const titleElement = document.querySelector('.profile__title');
 const typeElement = document.querySelector('.profile__subtitle');
-const nameFieldElement = document.querySelector('.popup__input_type_name');
-const typeFieldElement = document.querySelector('.popup__input_type_occupation');
+const nameField = document.getElementById('personName');
+const occupationField = document.getElementById('personOccupation');
+
+//popup place
+const popupPlace = document.getElementById('popupPlace');
+const editButtonBig = document.querySelector('.profile__button_size_big');
+const closeButtonPlace = document.getElementById('closeButtonPlace');
+const picNameField = document.getElementById('picName');
+const picURLField = document.getElementById('picURL');
+const list = document.querySelector('.photo-grid');
+const listItem = document.querySelector('.photo-grid__item');
+const listTitle = document.querySelector('.photo-grid__title');
+
 
 editButtonSmall.addEventListener('click', function(){
     popupName.classList.add('popup_open');
-    nameFieldElement.value = titleElement.textContent;
-    typeFieldElement.value = typeElement.textContent;
+    nameField.value = titleElement.textContent;
+    occupationField.value = typeElement.textContent;
 });
 
-function button (button, popup) {
+editButtonBig.addEventListener('click', function(){
+  popupPlace.classList.add('popup_open');
+  picNameField.value = 'Название';
+  picURLField.value = 'Ссылка на картинку';
+});
+
+function closePopup (button, popup) {
     button.addEventListener('click', function(){
         popup.classList.remove('popup_open');
     });
 };  
 
-button (closeButton, popupName);
-//closeButton.addEventListener('click', function(){
- //   popupName.classList.remove('popup_open');
-//});
+closePopup (closeButtonName, popupName);
+closePopup (closeButtonPlace, popupPlace);
+
 formElement.addEventListener('submit', function(event){
     event.preventDefault();
-    titleElement.textContent = nameFieldElement.value;
-    typeElement.textContent = typeFieldElement.value;
+    titleElement.textContent = nameField.value;
+    typeElement.textContent = occupationField.value;
     popupName.classList.remove('popup_open');
 });
 
-editButtonBig.addEventListener('click', function(){
-    popupPlace.classList.add('popup_open');
-    nameFieldElement.value = titleElement.textContent;
-    typeFieldElement.value = typeElement.textContent;
+formElement.addEventListener('submit', function(event){
+  event.preventDefault();
+  const copy = listItem.cloneNode(true);
+  list.prepend(copy);
+  //listTitle.textContent = nameField.value;
+  //typeElement.textContent = occupationField.value;
+  popupName.classList.remove('popup_open');
 });
+
+
 
