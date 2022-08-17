faf50f89d1898fde14f62dc669b27e1c59498bdf
Alina Kravchenko <krav-alina@yandex.ru>
2022-08-17 10:50:04 +0300

    edit

--- scripts/index.js
+++ scripts/index.js
@@ -29,7 +29,7 @@ const popupName = document.getElementById('popupName');
 const editButtonSmall = document.querySelector('.profile__button_size_small');
 const closeButtonName = document.getElementById('closeButtonName');
 const saveButton = document.querySelector('.popup__button');
-const formElement = document.querySelector('.popup__form');
+const formElement = document.getElementById('formName');
 const titleElement = document.querySelector('.profile__title');
 const typeElement = document.querySelector('.profile__subtitle');
 const nameField = document.getElementById('personName');
@@ -43,7 +43,7 @@ const picNameField = document.getElementById('picName');
 const picURLField = document.getElementById('picURL');
 const list = document.querySelector('.photo-grid');
 const listItem = document.querySelector('.photo-grid__item');
-const listTitle = document.querySelector('.photo-grid__title');
+const formPlace = document.getElementById('formPlace');
 
 
 editButtonSmall.addEventListener('click', function(){
@@ -74,11 +74,34 @@ formElement.addEventListener('submit', function(event){
     popupName.classList.remove('popup_open');
 });
 
-formElement.addEventListener('submit', function(event){
+
+const copy = listItem.cloneNode(true);
+formPlace.addEventListener('submit', function(event){
   event.preventDefault();
-  const copy = listItem.cloneNode(true);
+  let rectangle = null;
+  let name = null;
+  let photo = null;
+  for (let i = 0; i < copy.childNodes.length; i++) {
+      if (copy.childNodes[i].className == "photo-grid__photo") {
+        photo = copy.childNodes[i];
+        break;
+      }       
+  }
+  for (let i = 0; i < copy.childNodes.length; i++) {
+    if (copy.childNodes[i].className == "photo-grid__rectangle") {
+      rectangle = copy.childNodes[i];
+      break;
+    }        
+  }
+  for (let i = 0; i < rectangle.childNodes.length; i++) {
+    if (rectangle.childNodes[i].className == "photo-grid__title") {
+      name = rectangle.childNodes[i];
+      break;
+    }        
+  }
+  photo.src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg';
+  name.textContent = 'jghg';
   list.prepend(copy);
-  //listTitle.textContent = nameField.value;
   //typeElement.textContent = occupationField.value;
   popupName.classList.remove('popup_open');
 });
