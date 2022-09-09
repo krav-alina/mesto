1c8556d921d732174ed9c1e6e5838896947528e6
Alina Kravchenko <krav-alina@yandex.ru>
2022-09-09 21:04:10 +0300

    edit

--- scripts/validation.js
+++ scripts/validation.js
@@ -2,13 +2,13 @@ const showInputError = (formElement, inputElement, errorMessage) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.add('popup__input_type_error');
   errorElement.textContent = errorMessage;
-  errorElement.classList.add('popup__input-error_active');
+  errorElement.classList.add('popup__input_active');
 };
 
 const hideInputError = (formElement, inputElement) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.remove('popup__input_type_error');
-  errorElement.classList.remove('popup__input-error_active');
+  errorElement.classList.remove('popup__input_active');
   errorElement.textContent = '';
 };
 
